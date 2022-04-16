efec8d818c90dda1f47c9b416996f87f12b4c79f
Luke Wardle <l.wardle@live.co.uk>
2022-04-16 22:48:54 +0100

    Game finished

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Threading;
 
 namespace MatchGame
 {
@@ -11,38 +13,102 @@ namespace MatchGame
     /// </summary>
     public partial class MainWindow : Window
     {
+        DispatcherTimer timer = new DispatcherTimer();
+        int tenthsOfSecondsElapsed;
+        int matchesFound;
+
         public MainWindow()
         {
             InitializeComponent();
 
+            timer.Interval = TimeSpan.FromSeconds(.1);
+            timer.Tick += Timer_Tick;
             SetUpGame();
         }
 
+        //Timer counter for game timer increments until all pairs are found.
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            tenthsOfSecondsElapsed++;
+            timeTextBlock.Text = (tenthsOfSecondsElapsed / 10f).ToString("0.0s");
+            if (matchesFound == 8)
+            {
+                timer.Stop();
+                timeTextBlock.Text = timeTextBlock.Text + " - Play again?";
+            }
+        }
+
+
+        // The following code block has a collection of emojis, then calls the random method to place the emojis in random positions.
         private void SetUpGame()
         {
             List<string> animalEmoji = new List<string>()
             {
                 "😍", "😍",
-                "👌","👌",
-                "😒","😒",
+                "👻","👻",
+                "👽","👽",
                 "😎","😎",
-                "🤷‍♂️","🤷‍♂️",
-                "✌️","✌️",
-                "😢","😢",
-                "🤦‍♂️","🤦‍♂️",
+                "🐇","🐇",
+                "🦍","🦍",
+                "🐮","🐮",
+                "🦆","🦆"
             };
 
             Random random = new Random();
 
             foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
             {
-                int index = random.Next(animalEmoji.Count);
-                string nextEmoji = animalEmoji[index];
-                textBlock.Text = nextEmoji;
-                animalEmoji.RemoveAt(index);
+                if (textBlock.Name != "timeTextBlock")
+                {
+                    int index = random.Next(animalEmoji.Count);
+                    string nextEmoji = animalEmoji[index];
+                    textBlock.Text = nextEmoji;
+                    animalEmoji.RemoveAt(index);
+                }
             }
+
+            timer.Start();
+            tenthsOfSecondsElapsed = 0;
+            matchesFound = 0;
         }
 
+        TextBlock lastTextBlockClicked;
+        bool findingMatch = false;
 
+
+        //Below code runs on mouse down and checks if the user is selecting first or second card
+        //and hiding selection or pair or neither as appropriate.
+        private void TextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            TextBlock textBlock = sender as TextBlock;
+            if (findingMatch == false)
+            {
+                textBlock.Visibility = Visibility.Hidden;
+                lastTextBlockClicked = textBlock;
+                findingMatch = true;
+            }
+            else if (textBlock.Text == lastTextBlockClicked.Text)
+            {
+                matchesFound++;
+                textBlock.Visibility = Visibility.Hidden;
+                findingMatch = false;
+            }
+            else
+            {
+                lastTextBlockClicked.Visibility = Visibility.Visible;
+                findingMatch = false;
+            }
+
+        }
+
+        //Resets the game once all pairs have been matched.
+        private void TimeTextBlock_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (matchesFound == 8)
+            {
+
+                SetUpGame();
+            }
+        }
     }
 }
