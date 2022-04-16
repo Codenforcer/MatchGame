2c5bce305c8f3810c66c9642cb113a1d6b2f2658
Luke Wardle <l.wardle@live.co.uk>
2022-04-16 01:05:40 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            SetUpGame();
+        }
+
+        private void SetUpGame()
+        {
+            List<string> animalEmoji = new List<string>()
+            {
+                "😍", "😍",
+                "👌","👌",
+                "😒","😒",
+                "😎","😎",
+                "🤷‍♂️","🤷‍♂️",
+                "✌️","✌️",
+                "😢","😢",
+                "🤦‍♂️","🤦‍♂️",
+            };
+
+            Random random = new Random();
+
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int index = random.Next(animalEmoji.Count);
+                string nextEmoji = animalEmoji[index];
+                textBlock.Text = nextEmoji;
+                animalEmoji.RemoveAt(index);
+            }
+        }
+
+
+    }
+}
